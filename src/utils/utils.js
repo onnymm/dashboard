72fd3652996be1c8d7a99ef80fe11002d69afccd
onnymm <phr2807@gmail.com>
2024-07-09 13:38:54 -0700

    Añadidas funciones de transformación y adaptación de datos para componentes de gráficas

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -21,3 +21,71 @@ export const transformData = (data) => {
     // Se retorna el objeto
     return (series)
 }
+
+export const buildBarData = (
+    data, // Objeto de datos retornado del API
+    labelsName, // Variable de etiquetas de la gráfica de barras
+    datasetNames, // Variable de cada uno de los conjuntos de datos en el objeto
+    labels, // Nombres visibles en la gráfica para cada conjunto de datos
+    backgroundColors, // Colores de los conjuntos de datos
+    // Argumentos opcionales
+    xLabelsFormatter = undefined, // Formateo en las etiquetas del eje X
+    yLabelsFormatter = undefined, // Formateo los valores del eje Y
+) => {
+
+    // Inicialización del contenedor de datos
+    const series = {};
+    // Inicialización del contenedor de opciones
+    const options = optionsBuilder();
+    // Se asignan los nombres de las etiquetas
+    series.labels = data[labelsName];
+    // Se inicializa la matriz de conjuntos de datos
+    series.datasets = [];
+
+    // Iteración por cada uno de los conjuntos de datos
+    for (let i = 0; i < datasetNames.length; i++) {
+        // Se crea cada uno de los objetos
+        series.datasets[i] = {
+            // Nombre visible del conjunto de datos
+            label: labels[i],
+            // Variable contenedora del conjunto de datos
+            data: data[datasetNames[i]],
+            // Color del conjunto de datos
+            backgroundColor: backgroundColors[i]
+        };
+    }
+
+    // Configuración con argumentos opcionales
+    // Formateo de etiquetas en el eje X
+    if (xLabelsFormatter) {
+        series.labels = series.labels.map((value) => xLabelsFormatter(value))
+    }
+
+    // Formateo de etiquetas en el eje Y
+    if (yLabelsFormatter) {
+        options.scales.y.ticks.callback = yLabelsFormatter
+        console.log(options.scales.y.ticks.callback)
+    }
+
+    // Retorno del objeto a ingresar al componente de graficación
+    return {options, series}
+};
+
+export const dataFormatters = {
+    // Mostrar sólo el primer nombre en un String antes del espacio
+    onlyName: (text) => (text.slice(0, text.indexOf(" "))),
+    // Números a moneda nacional
+    toMXN: (num) => (num.toLocaleString('es-MX', {style: 'currency', currency: 'MXN'}))
+}
+
+const optionsBuilder = () => {
+    const options = {}
+
+    options.scales = {}
+    options.scales.x = {}
+    options.scales.y = {}
+    options.scales.x.ticks = {}
+    options.scales.y.ticks = {}
+
+    return options
+}
