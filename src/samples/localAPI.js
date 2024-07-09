cafe81ba5c4300730634fcf17f5a51313ad0684c
onnymm <phr2807@gmail.com>
2024-07-09 13:40:22 -0700

    Añadido archivo para conexión a API local, es provisional

--- src/samples/localAPI.js
+++ src/samples/localAPI.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 import { transformData } from "../utils/utils";
 
-const getBarData = async () => {
+const getBarData = async (stateSetter) => {
     // Se obtiene la información desde el API
     const response = await axios.get("http://192.168.1.89:8000/quotation_amounts")
     
     // Se retorna la información transformada
-    console.log(
+    stateSetter(
         transformData(response.data)
     )
 }
