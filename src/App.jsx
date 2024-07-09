155527ee56aa5bf93061dcd253902f9a82abe78d
onnymm <phr2807@gmail.com>
2024-07-09 16:27:22 -0700

    Arreglado App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useEffect } from 'react'
 import Sidebar from './components/sidebar/Sidebar'
-import getBarData from './samples/localAPI'
 
 const App = () => {
-	useEffect(() => {
-		getBarData()
-	}, [])
 
 	return <Sidebar />
 }
