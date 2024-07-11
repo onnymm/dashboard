8a8ed9b9792f2b45df47061698844b18a93d60de
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-11 09:25:52 -0700

    Se agregaron más estilos globales

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,27 @@ export default {
 				'sidebar-background': '#1B2436',
 				'sidebar-section-title': '#8A99AF',
 				'sidebar-section-hover': '#333A48',
-				'sidebar-section': '#DEE4EE'
+				'sidebar-section': '#DEE4EE',
+
+				'content-background': '#E1E8F6',
+
+				'navbar-background': '#FFFFFF',
+				'navbar-icons': '#78909C',
+				'navbar-icons-hover': '#1565C0',
+
+				'darkmode-switcher': '#FFFFFF',
+				'darkmode-switcher-background': '#D6DBDF'
+			},
+			width: {
+				265: '265px'
+			},
+			height: {
+				4000: '4000px'
+			},
+			boxShadow: {
+				'darkmode-switcher-s': '0px 1px 2px 0px rgba(0, 0, 0, 0.6)',
+				'darkmode-switcher-background-s':
+					'inset 0px 1px 2px 0px rgba(0, 0, 0, 0.45)'
 			}
 		}
 	},
