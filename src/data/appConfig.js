ea4b52876cb3ee3d9c31772d98096c4cb496eb35
onnymm <phr2807@gmail.com>
2024-07-12 14:31:43 -0700

    Añadido directorio y archivo de configuración para modoularizar el esquema de datos

---
+++ src/data/appConfig.js
@@ -0,0 +1,57 @@
+import { redPalette } from "../constants/colors";
+import { dataFormatters } from "../utils/utils";
+
+// Dominios locales para obtener la información
+export const defaultDomain = "localDomain";
+
+// Configuración de las gráficas
+export const dashboardData = {
+    charts: [
+        {
+            endpoint: "quotation_amounts",
+            chartType: "bar",
+            name: "Cotizaciones por vendedora",
+            labelsName: "user_name",
+            datasetNames: ["amount_untaxed"],
+            labels: ["Cotizaciones"],
+            backgroundColors: redPalette,
+            xLabelsFormatter: dataFormatters.onlyName,
+            yLabelsFormatter: dataFormatters.toMXN,
+        },
+        {
+            endpoint: "quotation_amounts",
+            chartType: "line",
+            name: "Cotizaciones por vendedora",
+            labelsName: "user_name",
+            datasetNames: ["amount_untaxed"],
+            labels: ["Cotizaciones"],
+            backgroundColors: redPalette[3],
+            backgroundOpacity: 75,
+            borderColors: redPalette[3],
+            xLabelsFormatter: dataFormatters.onlyName,
+            yLabelsFormatter: dataFormatters.toMXN,
+        },
+        {
+            endpoint: "quotation_amounts",
+            chartType: "pie",
+            name: "Cotizaciones por vendedora",
+            labelsName: "user_name",
+            datasetNames: ["amount_untaxed"],
+            labels: ["Cotizaciones"],
+            backgroundColors: redPalette,
+        },
+        {
+            chartType: "line",
+            endpoint: "monthly_total_amounts",
+            name: "No hace nada",
+            labelsName: "month",
+            datasetNames: ["total_amount"],
+            labels: ["Cotizaciones"],
+            borderColors: ["#FF0000", "#0000FF"],
+            backgroundColors: ["#FF0000", "#0000FF"],
+            backgroundOpacity: 75,
+            yLabelsFormatter: dataFormatters.toMXN,
+            strat: "warehouse"
+        },
+    ]
+}
