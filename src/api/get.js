7c964f6febb24365646dde3a4ec76029098453dc
onnymm <phr2807@gmail.com>
2024-07-12 14:09:55 -0700

    Cambiado el esquema de manejo de APIs

---
+++ src/api/get.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+import { domains } from "../constants/backendDomains"
+import { defaultDomain } from "../data/appConfig"
+
+export const getChartData = async (
+    stateSetter, // Función useState para cambio de estado
+    endpoint, // Endpoint del cual se obtendrán los datos
+    domain, // Dominio específico en caso de usar uno diferente al default
+) => {
+
+    // Uso de dominio por defecto en caso de no haber sido provisto
+    if (!domain) {
+        domain = defaultDomain
+    }
+
+    // Se obtiene la información desde el API
+    const response = await axios.get(`${domains[domain]}${endpoint}`)
+    // Se retorna la información transformada
+    stateSetter(response.data)
+}
