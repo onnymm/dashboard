63eedcb0dc36f89237597dd317abed54176485f3
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-11 17:16:37 -0700

    Reorganización del componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import Feed from './components/feed/Feed'
+import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
+import SidebarToggle from './components/sidebar/SidebarToggle'
 
 const App = () => {
+	const [openSidebar, setOpenSidebar] = useState(false)
+
 	return (
-		<div className='flex'>
-			<Sidebar />
-			<Feed />
-		</div>
+		<>
+			<div className='h-screen overflow-y-auto'>
+				<div className='sticky top-0 flex flex-row h-20 select-none shadow bg-navbar-background'>
+					<Sidebar isOpen={openSidebar} />
+					<SidebarToggle
+						openSidebar={openSidebar}
+						setOpenSidebar={setOpenSidebar}
+					/>
+					<Navbar />
+				</div>
+				<div className=''>
+					<Feed />
+				</div>
+			</div>
+		</>
 	)
 }
 
