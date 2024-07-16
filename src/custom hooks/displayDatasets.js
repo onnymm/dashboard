558bed0089eedf8e51a27b96e5bbde4aaf8f2a81
onnymm <phr2807@gmail.com>
2024-07-16 10:39:45 -0700

    Añadido custom hook para mostrar/ocultar conjuntos de datos en gráficas

---
+++ src/custom hooks/displayDatasets.js
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+export const useCategoryDataset = (datasets) => {
+    // Inicialización del objeto interno de mapeo de conjuntos de datos
+    const datasetsToDisplay = {}
+
+    // Mapeo de conjuntos de datos
+	for (let i = 0; i < datasets.length; i++) {
+		datasetsToDisplay[i] = true
+	}
+
+    // Inicialización de matriz contenedora de todos los índices de conjuntos de datos
+    const datasetsKeys = Object.keys(datasetsToDisplay)
+
+    // Inicialización de estado interno de mapeo de conjuntos de datos
+	const [displayedDatasets, setDisplayedDatasets] = useState(datasets)
+    // Inicialización de estado interno de conjuntos de datos a mostrar
+	const [toggleDatasets, setToggleDatasets] = useState(datasetsToDisplay)
+
+    // Inicialización de función de estado personalizada
+	const setDatasets = (keyToToggle) => {
+
+        // Copia del estado de mapeo de conjuntos de datos para poder usarlo tras un cambio
+		const newToggleDatasets = {...toggleDatasets}
+        // Inicialización de nueva matriz de conjuntos de datos a usar en el cambio de estado
+		const newDatasets = []
+
+        // Iteración por las llaves del objeto de mapeo de conjuntos de datos para usar sus índices
+		datasetsKeys.forEach(
+			(datasetKey) => {
+                // Se cambia el estado booleano del item si coincide con la llave provista
+				if ( datasetKey === keyToToggle ) {
+					newToggleDatasets[datasetKey] = !newToggleDatasets[datasetKey]
+				}
+
+                // Se toman los conjuntos de datos donde su índice sea verdadero (Mostrarse)
+				if ( newToggleDatasets[datasetKey] ) {
+					newDatasets.push(datasets[datasetKey])
+				}
+			}
+		)
+
+        // Actualización de estado del mapeo de conjuntos de datos
+		setToggleDatasets(newToggleDatasets)
+        // Actualización de estado de conjuntos de datos mostrados
+		setDisplayedDatasets(newDatasets)
+	}
+
+	return [displayedDatasets, setDatasets, datasetsKeys]
+}
