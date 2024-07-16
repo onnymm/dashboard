54464abfef6c05cd36557cb5f0722005b2d94011
onnymm <phr2807@gmail.com>
2024-07-16 13:36:18 -0700

    Añadida función para seleccionar el tipo de manejo de mostrar/ocultar datasets

---
+++ src/utils/manageDatasets.js
@@ -0,0 +1,7 @@
+import { useCategoryDataset } from "../custom hooks/displayDatasets"
+
+export const setDatasetSelection = (dataContainer) => {
+    if ( dataContainer.series.datasets.length > 1 ) {
+        return (useCategoryDataset)
+    }
+}
