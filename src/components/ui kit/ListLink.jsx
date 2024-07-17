a2731e0e0dddd72af2887a6f8ea3ea098b4fa560
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-17 15:36:52 -0700

    Optimización de componente

--- src/components/ui kit/ListLink.jsx
+++ src/components/ui kit/ListLink.jsx
@@ -1,19 +1,24 @@
-import { FaceFrownIcon, UserIcon } from '@heroicons/react/24/outline'
+import {
+	ArrowLeftStartOnRectangleIcon,
+	FaceFrownIcon,
+	UserIcon
+} from '@heroicons/react/24/outline'
 import { NavLink } from 'react-router-dom'
 
 const ICONS = {
 	user: <UserIcon />,
-	face_frown: <FaceFrownIcon />
+	face_frown: <FaceFrownIcon />,
+	log_out: <ArrowLeftStartOnRectangleIcon />
 }
 
 const ListLink = ({ icon, label, route }) => {
 	return (
 		<NavLink
-			className='flex gap-2 text-sm hover:bg-gray-300 transition duration-300 dark:text-white opacity-80 p-2 rounded-md'
+			className='flex gap-2 text-sm dark:hover:bg-slate-800 hover:bg-gray-300 opacity-80 p-2 rounded-md text-black dark:text-white transition duration-300'
 			to={route}
 		>
 			<div className='size-5'>{ICONS[icon]}</div>
-			{label}
+			<span>{label}</span>
 		</NavLink>
 	)
 }
