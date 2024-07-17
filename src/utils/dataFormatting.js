4bdc7933667e624da49c5d19fda4759eca2d402d
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-17 15:38:18 -0700

    Se añadió función para formatear strings

--- src/utils/dataFormatting.js
+++ src/utils/dataFormatting.js
@@ -1,51 +1,52 @@
 export const stratificateData = (data, varGroup, varValue, labelsName) => {
-    // Se transforma data de un diccionario a una matriz
-    data = Object.values(data)
-    // Se inicializa la matriz a retornar
-    const datasets = []
-    // Se inicializa el contenedor de los nombres de grupo en el conjunto de datos
-    const groups = []
-    // Se inicializa el contenedor de los nombres de etiquetas
-    const labels = []
+	// Se transforma data de un diccionario a una matriz
+	data = Object.values(data)
+	// Se inicializa la matriz a retornar
+	const datasets = []
+	// Se inicializa el contenedor de los nombres de grupo en el conjunto de datos
+	const groups = []
+	// Se inicializa el contenedor de los nombres de etiquetas
+	const labels = []
 
-    // Se obtienen todos los nombres de grupo del conjunto de datos
-    data.forEach(
-        (sample) => {
-            // Si el nombre no existe en la matriz, se agrega
-            if ( groups.indexOf(sample[varGroup]) === -1 ) {
-                groups.push(sample[varGroup])
-            }
-            if ( labels.indexOf(sample[labelsName]) === -1 ) {
-                labels.push(sample[labelsName])
-            }
-        }
-    )
+	// Se obtienen todos los nombres de grupo del conjunto de datos
+	data.forEach(sample => {
+		// Si el nombre no existe en la matriz, se agrega
+		if (groups.indexOf(sample[varGroup]) === -1) {
+			groups.push(sample[varGroup])
+		}
+		if (labels.indexOf(sample[labelsName]) === -1) {
+			labels.push(sample[labelsName])
+		}
+	})
 
-    // Se realiza la búsqueda y agrupación por todos los nombres de grupo
-    groups.forEach(
-        // Iteración por nombres de grupo
-        (name) => {
-            // Se crea un conjunto de datos vacío
-            const dataset = {}
-            // Se designa el nombre como etiqueta del conjunto de datos
-            dataset.label = name
-            // Se inicializa el contenedor de valores del conjunto de datos
-            dataset.data = []
+	// Se realiza la búsqueda y agrupación por todos los nombres de grupo
+	groups.forEach(
+		// Iteración por nombres de grupo
+		name => {
+			// Se crea un conjunto de datos vacío
+			const dataset = {}
+			// Se designa el nombre como etiqueta del conjunto de datos
+			dataset.label = name
+			// Se inicializa el contenedor de valores del conjunto de datos
+			dataset.data = []
 
-            // Iteración por cada valor del conjunto de datos
-            data.forEach(
-                (sample) => {
-                    // Se agrega el valor del conjunto de datos entrante si pertenece al grupo del nombre
-                    if ( sample[varGroup] === name ) {
-                        dataset.data.push(sample[varValue])
-                    }
-                }
-            )
+			// Iteración por cada valor del conjunto de datos
+			data.forEach(sample => {
+				// Se agrega el valor del conjunto de datos entrante si pertenece al grupo del nombre
+				if (sample[varGroup] === name) {
+					dataset.data.push(sample[varValue])
+				}
+			})
 
-            // Finalizada la iteración por el conjunto de datos entrante, se añade el nuevo conjunto de datos al objeto a retornar
-            datasets.push(dataset)
-        }
-    )
+			// Finalizada la iteración por el conjunto de datos entrante, se añade el nuevo conjunto de datos al objeto a retornar
+			datasets.push(dataset)
+		}
+	)
 
-    return [ datasets, labels ]
+	return [datasets, labels]
+}
+
+export const capitalizeFirstLetter = str => {
+	if (str.length === 0) return str
+	return str.charAt(0).toUpperCase() + str.slice(1)
 }
