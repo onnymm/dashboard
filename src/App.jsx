d119ac403b56cfefeae11f379eb0a6f60c992626
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-22 08:41:52 -0700

    Reestructuración del componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
+import { useState } from 'react'
 import Feed from './components/feed/Feed'
 import Navbar from './components/navbar/Navbar'
+import { AppContext } from './contexts/AppContexts'
 
 const App = () => {
+	const [sidebarIsOpen, setSidebarIsOpen] = useState(false)
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
 	return (
-		<>
-			<div className='h-screen overflow-y-auto'>
+		<div className='relative h-screen overflow-y-auto overflow-x-hidden'>
+			<AppContext.Provider value={{ sidebarIsOpen, setSidebarIsOpen }}>
 				<div className='transition-color sticky top-0 flex h-20 select-none flex-row bg-navbar-background shadow duration-300 dark:bg-navbar-background-d'>
 					<Navbar />
 				</div>
 				<div
-					className='transition-color bg-feed-background duration-300 dark:bg-feed-background-d'
+					className='transition-color absolute -z-9 w-full bg-slate-100 duration-300 dark:bg-feed-background-d'
 					style={{
 						minHeight: 'calc(100% - 5rem)'
 					}}
 				>
 					<Feed />
 				</div>
-			</div>
-		</>
+			</AppContext.Provider>
+		</div>
 	)
 }
 
