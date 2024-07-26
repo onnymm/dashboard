05b1742433b9eec76282a02097a034ab82347c4e
onnymm <phr2807@gmail.com>
2024-07-26 13:07:10 -0700

    Refactorizado plug-in htmlLegend para compatibilidad con modo oscuro

--- src/plugins/htmlLegend.js
+++ src/plugins/htmlLegend.js
@@ -26,74 +26,101 @@ const getOrCreateLegendList = (chart, id) => {
     };
     
 
+const createLabels = (ul, chart) => {
+    // Obtención de etiquetas de los conjuntos de datos de la gráfica
+    const items = chart.options.plugins.legend.labels.generateLabels(chart);
+
+    items.forEach(
+        (item) => {
+            // Se crea un item de lista
+            let li = document.createElement('li');
+
+            // Asignación de estilos de configuración
+            const {extension} = chart.config._config.options;
+            // Creación del elemento de la lista
+            li = chartStyleSetter(extension, li, CHARTS_SETTINGS.LABELS_LIST);
+
+            li.onclick = () => {
+                // Se obtiene la declaración del tipo de gráfica
+                const {type} = chart.config;
+
+                // Validación del tipo de gráfica
+                if (type === CHART_TYPES.PIE || type === CHART_TYPES.DOUGHNUT) {
+                    // Asignación de manipulación de vistas por categoría en el conjunto de datos
+                    chart.toggleDataVisibility(item.index);
+                } else {
+                    // Asignación de manipulación de vistas por conjunto de datos
+                    chart.setDatasetVisibility(item.datasetIndex, !chart.isDatasetVisible(item.datasetIndex));
+                }
+                // Actualización de la gráfica
+                chart.update();
+            };
+
+            // Caja de color de la etiqueta
+            let boxSpan = document.createElement('span');
+
+            // Asignación de estilos provenientes de las propiedades del conjunto de datos
+            boxSpan.style.background = item.fillStyle;
+            boxSpan.style.borderColor = item.strokeStyle;
+            
+            // Asignación de estilos de configuración
+            boxSpan = chartStyleSetter(extension, boxSpan, CHARTS_SETTINGS.LEGEND_BOX);
+            
+            // Texto de la etiqueta
+            const textContainer = document.createElement('p');
+            // Asignación de estilos provenientes de las propiedades del conjunto de datos
+            textContainer.style.textDecoration = item.hidden ? 'line-through' : '';
+            textContainer.style.color = chart.options.font.color;
+            // Asignación de clases CSS
+            textContainer.classList.add("transition")
+            textContainer.classList.add("duration-300")
+
+            // Asignación del nombre del conjunto de datos o categoría del conjunto de datos
+            const text = document.createTextNode(item.text);
+            textContainer.appendChild(text);
+
+            // Se agregan la caja de color y el texto de la etiqueta al elemento de la lista
+            li.appendChild(boxSpan);
+            li.appendChild(textContainer);
+
+            // Se agrega el elemento de la lista a la lista
+            ul.appendChild(li);
+        }
+    );
+}
+
+const updateLabels = (ul, chart) => {
+    // Obtención de etiquetas de los conjuntos de datos de la gráfica
+    const items = chart.options.plugins.legend.labels.generateLabels(chart);
+
+    // Obtención de elementos en la lista
+    const ulElements = ul.children
+
+    items.forEach(
+        (item, index) => {
+            // Asignación de estilos provenientes de las propiedades del conjunto de datos
+            ulElements[index].style.textDecoration = item.hidden ? 'line-through' : '';
+            // ulElements[index].style.color = chart.options.font.color;
+            const p = ulElements[index].querySelector('p')
+            p.style.color = chart.options.font.color;
+        }
+    );
+}
+
 // Plug-in de renderización etiquetas en div
 const htmlLegendPlugin = {
     id: 'htmlLegend',
     afterUpdate(chart, args, options) {
+        // Creación u obtención del elemento de lista del contenedor div
         const ul = getOrCreateLegendList(chart, options.containerID);
 
-        // Reseteo de vista de conjuntos de datos eliminando el contenedor de etiquetas desactualizado
-        while (ul.firstChild) {
-            ul.firstChild.remove();
+        // Actualización de las etiquetas en caso de existir
+        if ( ul.children.length ) {
+            updateLabels(ul, chart)
+        // Creación de las etiquetas si la lista está vacía
+        } else {
+            createLabels(ul, chart)
         }
-
-        // Obtención de etiquetas de los conjuntos de datos de la gráfica
-        const items = chart.options.plugins.legend.labels.generateLabels(chart);
-        console.log(chart.options.plugins.legend)
-
-        items.forEach(
-            (item) => {
-                // Se crea un item de lista
-                let li = document.createElement('li');
-
-                // Asignación de estilos de configuración
-                const {extension} = chart.config._config.options;
-                li = chartStyleSetter(extension, li, CHARTS_SETTINGS.LABELS_LIST);
-
-                li.onclick = () => {
-                    // Se obtiene la declaración del tipo de gráfica
-                    const {type} = chart.config;
-
-                    // Validación del tipo de gráfica
-                    if (type === CHART_TYPES.PIE || type === CHART_TYPES.DOUGHNUT) {
-                        // Asignación de manipulación de vistas por categoría en el conjunto de datos
-                        chart.toggleDataVisibility(item.index);
-                    } else {
-                        // Asignación de manipulación de vistas por conjunto de datos
-                        chart.setDatasetVisibility(item.datasetIndex, !chart.isDatasetVisible(item.datasetIndex));
-                    }
-                    // Actualización de la gráfica
-                    chart.update();
-                };
-
-                // Caja de color de la etiqueta
-                let boxSpan = document.createElement('span');
-
-                // Asignación de estilos provenientes de las propiedades del conjunto de datos
-                boxSpan.style.background = item.fillStyle;
-                boxSpan.style.borderColor = item.strokeStyle;
-                
-                // Asignación de estilos de configuración
-                boxSpan = chartStyleSetter(extension, boxSpan, CHARTS_SETTINGS.LEGEND_BOX);
-                
-                // Texto de la etiqueta
-                const textContainer = document.createElement('p');
-                // Asignación de estilos provenientes de las propiedades del conjunto de datos
-                textContainer.style.textDecoration = item.hidden ? 'line-through' : '';
-                textContainer.style.color = chart.options.font.color;
-
-                // Asignación del nombre del conjunto de datos o categoría del conjunto de datos
-                const text = document.createTextNode(item.text);
-                textContainer.appendChild(text);
-
-                // Se agregan la caja de color y el texto de la etiqueta al elemento de la lista
-                li.appendChild(boxSpan);
-                li.appendChild(textContainer);
-
-                // Se agrega el elemento de la lista a la lista
-                ul.appendChild(li);
-            }
-        );
     }
 };
 
