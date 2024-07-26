41724d3d8938c2d888a551573d5a70004d2f2cb1
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-26 09:03:58 -0700

    Se añadieron páginas placeholder

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,13 +2,19 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 import Analytics from '../components/pages/analytics/Analytics'
 import Ecommerce from '../components/pages/ecommerce/Ecommerce'
+import Homepage from '../components/pages/homepage/Homepage'
 import Profile from '../components/pages/profile/Profile'
+import ToDo from '../components/pages/to do/ToDo'
 
 const Router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
 		children: [
+			{
+				path: '',
+				element: <Homepage />
+			},
 			{
 				path: 'profile',
 				element: <Profile />
@@ -20,6 +26,10 @@ const Router = createBrowserRouter([
 			{
 				path: 'analytics',
 				element: <Analytics />
+			},
+			{
+				path: 'to-do',
+				element: <ToDo />
 			}
 		]
 	}
