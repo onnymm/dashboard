c4ba36825d300ba3aed774c30bdd6790c8300c5b
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-01 14:00:01 -0700

    Ajustes de estilo

--- src/components/ui kit/SearchIcon.jsx
+++ src/components/ui kit/SearchIcon.jsx
@@ -1,10 +1,23 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
+import { useState } from 'react'
 
 const SearchIcon = ({ handleClick }) => {
+	const [isAnimating, setIsAnimating] = useState(false)
+
+	const handleTaskAndAnimation = e => {
+		setIsAnimating(true)
+		handleClick(e)
+	}
+
+	const handleAnimationEnd = () => {
+		setIsAnimating(false)
+	}
+
 	return (
 		<button
-			className='$ px-2 opacity-60 transition duration-300 hover:text-navbar-icons-hover-d hover:opacity-100 dark:text-white dark:hover:text-navbar-icons-hover-d'
-			onClick={e => handleClick(e)}
+			className={`${isAnimating && 'sm:animate-bars3-click-sm'} px-2 opacity-60 transition duration-300 hover:text-navbar-icons-hover-d hover:opacity-100 dark:text-white dark:hover:text-navbar-icons-hover-d`}
+			onClick={e => handleTaskAndAnimation(e)}
+			onAnimationEnd={handleAnimationEnd}
 		>
 			<MagnifyingGlassIcon className='size-5' />
 		</button>
