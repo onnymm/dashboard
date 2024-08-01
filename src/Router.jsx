5863417b6b0afea130b688ca6d5bc92f53c25791
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-01 16:19:48 -0700

    Ruta wildcard y fallback de error añadidos

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import Analytics from './routes/analytics/Analytics'
 import Ecommerce from './routes/ecommerce/Ecommerce'
+import ErrorPage from './routes/error elements/ErrorPage'
 import Homepage from './routes/homepage/Homepage'
 import Profile from './routes/profile/Profile'
 import ToDo from './routes/to do/ToDo'
@@ -11,30 +12,36 @@ const Router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
+		errorElement: <ErrorPage />,
 		children: [
 			{
-				path: '',
-				element: <Homepage />
-			},
-			{
-				path: 'profile',
-				element: <Profile />
-			},
-			{
-				path: 'ecommerce',
-				element: <Ecommerce />
-			},
-			{
-				path: 'analytics',
-				element: <Analytics />
-			},
-			{
-				path: 'to-do',
-				element: <ToDo />
-			},
-			{
-				path: 'ui-tests',
-				element: <UiTests />
+				children: [
+					{ index: true, element: <Homepage /> },
+					{
+						path: 'profile',
+						element: <Profile />
+					},
+					{
+						path: 'ecommerce',
+						element: <Ecommerce />
+					},
+					{
+						path: 'analytics',
+						element: <Analytics />
+					},
+					{
+						path: 'to-do',
+						element: <ToDo />
+					},
+					{
+						path: 'ui-tests',
+						element: <UiTests />
+					},
+					{
+						path: '*',
+						element: <ErrorPage /> // Ensure this wildcard path is included to catch all undefined routes
+					}
+				]
 			}
 		]
 	}
