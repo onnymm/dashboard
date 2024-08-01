d0005370b0c1d471b1a47833c6416b5150f7c37a
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-01 16:21:31 -0700

    Se removió el componente App, pues era innecesario

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
 import Router from './Router.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={Router}>
-        <App />
-    </RouterProvider> 
+    <React.StrictMode>
+        <RouterProvider router={Router}/>
+    </React.StrictMode>
 )
