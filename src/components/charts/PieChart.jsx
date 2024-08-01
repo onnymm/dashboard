936c19059ee48707ffd6f37da16d8f61c25c3cdf
onnymm <phr2807@gmail.com>
2024-08-01 14:31:05 -0700

    Optimizado el mapeo de componentes de gráficas y eliminados componentes redundantes

--- src/components/charts/PieChart.jsx
+++
@@ -1,9 +0,0 @@
-import { Pie } from 'react-chartjs-2'
-
-const PieChart = ({ dataContainer = undefined }) => {
-	if (dataContainer) {
-		return <Pie data={dataContainer.series} options={dataContainer.options} />
-	}
-}
-
-export default PieChart
