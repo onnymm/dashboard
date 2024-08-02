79a1b9ea1289ae693be593bbce89a7ea731a6995
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-01 17:20:20 -0700

    Removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import Router from './Router.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
         <RouterProvider router={Router}/>
-    </React.StrictMode>
 )
