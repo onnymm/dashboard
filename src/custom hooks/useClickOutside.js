3dfba2ac0ab82a764dc97e219df961f83d050eb3
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-05 10:18:52 -0700

    Comentarios añadidos

--- src/custom hooks/useClickOutside.js
+++ src/custom hooks/useClickOutside.js
@@ -1,17 +1,21 @@
 import { useEffect, useRef } from 'react'
 
-export let useClickOutside = handler => {
-	let domNode = useRef()
+// Recibe una función
+export const useClickOutside = handler => {
+	let domNode = useRef() // Genera una referencia al componente/nodo DOM que lo contenga
 
+	// Observa al nodo
 	useEffect(() => {
 		let maybeHandler = e => {
+			// Si un evento se genera, y el nodo no contiene al target del evento
 			if (!domNode.current.contains(e.target)) {
-				handler()
+				handler() // Ejecuta la función
 			}
 		}
 
-		document.addEventListener('mousedown', maybeHandler)
+		document.addEventListener('mousedown', maybeHandler) // Se agrega un listener para mousedown
 
+		// Borra el listener en caso de que el componente se desmonte
 		return () => {
 			document.removeEventListener('mousedown', maybeHandler)
 		}
