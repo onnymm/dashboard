b5295620a114528ade5a10821bb7a872601b721a
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-05 08:55:02 -0700

    Detalles añadidos

--- src/routes/error elements/ErrorPage.jsx
+++ src/routes/error elements/ErrorPage.jsx
@@ -2,18 +2,18 @@ import { HomeIcon } from '@heroicons/react/16/solid'
 import { Link, useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
-	const error = useRouteError() // Devuelve el error arrojado
+	const error = useRouteError() // Retrieves the thrown error
 
 	console.error(error)
 
 	return (
-		<div className='flex flex-grow flex-col items-center justify-center gap-2 opacity-90 dark:text-white dark:opacity-80'>
+		<div className='flex flex-grow flex-col items-center justify-center gap-2 py-10 opacity-90 dark:text-white dark:opacity-80'>
 			<h1 className='my-2 text-5xl font-bold'>Oops!</h1>
 			<p className='w-7/12 text-center font-medium sm:w-auto'>
-				Sorry, an unexpected error has ocurred.
+				Sorry, an unexpected error has occurred.
 			</p>
-			<p className='opacity-70 dark:text-white'>
-				<i>{error || 'Not found'}</i>
+			<p className='w-1/4 overflow-y-auto text-center opacity-70 dark:text-white'>
+				<i>{error instanceof Error ? error.message : 'Not found'}</i>
 			</p>
 			<Link className='flex gap-2 rounded-md border bg-blue-600 p-2 text-white shadow transition duration-300 hover:-translate-y-1 hover:bg-blue-500'>
 				<HomeIcon className='size-5' />
