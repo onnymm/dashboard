8c495ef86ed6682279599ee0978dfde6ef1e0114
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-05 16:12:00 -0700

    Cambios al elemento de error

--- src/routes/error elements/ErrorPage.jsx
+++ src/routes/error elements/ErrorPage.jsx
@@ -1,8 +1,9 @@
 import { HomeIcon } from '@heroicons/react/16/solid'
-import { Link, useRouteError } from 'react-router-dom'
+import { Link, useLocation, useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
-	const error = useRouteError() // Retrieves the thrown error
+	const error = useRouteError() // Recibe el error arrojado
+	const lowercasedPath = useLocation().pathname.toLowerCase().substring(1) // Extrae path sin slash (/)
 
 	console.error(error)
 
@@ -15,10 +16,13 @@ const ErrorPage = () => {
 			<p className='w-1/4 overflow-x-auto text-center opacity-70 dark:text-white'>
 				<i>{error instanceof Error ? error.message : 'Not found'}</i>
 			</p>
-			<Link className='flex gap-2 rounded-md bg-blue-600 px-4 py-2 text-white shadow transition duration-300 hover:-translate-y-1 hover:bg-blue-500'>
-				<HomeIcon className='size-5' />
-				<span>Take me home</span>
-			</Link>
+			{!lowercasedPath == '' && (
+				// Si el path no está vacío (no estamos en el home)...
+				<Link className='flex gap-2 rounded-md bg-blue-600 px-4 py-2 text-white shadow transition duration-300 hover:-translate-y-1 hover:bg-blue-500'>
+					<HomeIcon className='size-5' />
+					<span>Take me home</span>
+				</Link>
+			)}
 		</div>
 	)
 }
--- src/routes/ui tests/UiTests.jsx
+++ src/routes/ui tests/UiTests.jsx
@@ -1,9 +1,13 @@
+import { FallbackContentList } from '../../components/navbar/right side/action buttons/FallbackContentList'
 import FallbackProfileNavList from '../../components/navbar/right side/profile/FallbackProfileNavList'
 
 const UiTests = () => {
 	return (
 		<div className='flex flex-grow items-center justify-center text-center'>
 			<FallbackProfileNavList />
+			<div className='w-80'>
+				<FallbackContentList numberOfElements={3} />
+			</div>
 		</div>
 	)
 }
