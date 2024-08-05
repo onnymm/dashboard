7ac77e7c65f34ec38a2b0da813d8cb8ff1304397
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-05 08:57:06 -0700

    Optimización

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import Router from './Router.jsx'
+import Router from './Router'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
