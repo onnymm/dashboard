180f9fd1f13e414dcf87636f1b0335430a0b6788
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-06 10:41:18 -0700

    Se agregó placeholder de carga para imágen

--- src/components/ui kit/Message.jsx
+++ src/components/ui kit/Message.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
+
 const Message = ({ avatar, header, content }) => {
+	const [isLoaded, setIsLoaded] = useState(false)
+
+	const handleLoad = () => {
+		setIsLoaded(true)
+	}
+
 	return (
 		<div className='transition-color flex h-min items-center gap-3 overflow-hidden p-4 text-left text-xs duration-300 dark:text-white'>
 			<div className='w-12 flex-none'>
-				<img
-					src={avatar ? avatar : './default_profile.webp'}
-					className='rounded-full'
-				/>
+				<div className='relative size-12'>
+					{!isLoaded && (
+						<div className='absolute size-full animate-pulse rounded-full bg-slate-300 dark:bg-slate-500' />
+					)}
+					<img
+						src={avatar ? avatar : './default_profile.webp'}
+						className='size-full rounded-full'
+						onLoad={handleLoad}
+					/>
+				</div>
 			</div>
 			<div>
 				<span className='opacity-90 dark:opacity-90'>
