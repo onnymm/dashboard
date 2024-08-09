9fc56144a7e9b07a1e6ab72d3afecd75a9616187
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-09 13:06:34 -0700

    Bug fixes

--- src/components/common/navbar/left side/NavbarSearch.jsx
+++ src/components/common/navbar/left side/NavbarSearch.jsx
@@ -4,14 +4,16 @@ import { AppContext } from '../../../../contexts/AppContexts'
 import { useClickOutside } from '../../../../custom hooks/useClickOutside'
 import { useScreenWidth } from '../../../../custom hooks/useScreenWidth'
 import { thresholdForWideScreen } from '../../../../data/appConfig'
-import SearchIcon from '../../../ui kit/SearchIcon'
+import SearchButton from '../../../ui kit/SearchButton'
 
 const NavbarSearch = () => {
 	const { search: initialSearch } = useLoaderData()
-	const [search, setSearch] = useState(initialSearch)
+	const [search, setSearch] = useState(initialSearch || '')
 	const [bottomIsDisplayed, setBottomIsDisplayed] = useState(false)
 	const { sidebarIsLocked } = useContext(AppContext)
 	const screenIsWide = useScreenWidth(thresholdForWideScreen)
+
+	console.log(bottomIsDisplayed)
 
 	useEffect(() => {
 		if (screenIsWide && bottomIsDisplayed) {
@@ -41,21 +43,33 @@ const NavbarSearch = () => {
 			<div
 				className={`${sidebarIsLocked && screenIsWide ? 'w-40' : 'w-14 sm:w-20'} flex transition-width duration-500`}
 			/>
-			<Form className='relative flex' onSubmit={handleSubmit} ref={domNode}>
-				<div
-					className={`${!bottomIsDisplayed ? 'opacity-100' : 'pointer-events-none opacity-0'} flex transition duration-100`}
-				>
-					<SearchIcon
-						handleClick={screenIsWide ? hideMiniSearch : displayMiniSearch}
+			<div
+				className={`${!bottomIsDisplayed ? 'opacity-100' : 'pointer-events-none opacity-0'} flex transition duration-100`}
+			>
+				{!screenIsWide && (
+					<SearchButton
+						handleClick={e =>
+							screenIsWide ? hideMiniSearch(e) : displayMiniSearch(e)
+						}
 					/>
-				</div>
+				)}
+			</div>
+			<Form className='relative flex' onSubmit={handleSubmit} ref={domNode}>
+				{screenIsWide && (
+					<div
+						className={`${!bottomIsDisplayed ? 'opacity-100' : 'pointer-events-none opacity-0'} flex transition duration-100`}
+					>
+						<SearchButton />
+					</div>
+				)}
 				<input
 					name='query'
 					type='search'
 					value={search}
+					autoComplete='off'
 					onChange={e => setSearch(e.target.value)}
 					placeholder='Type to search...'
-					className={`${bottomIsDisplayed ? 'absolute top-12 rounded-md bg-white p-4 shadow-back dark:bg-navbar-icons-background-d' : 'hidden bg-transparent px-2 dark:opacity-80 md:block'} text-sm focus:outline-none dark:text-white dark:placeholder-white dark:placeholder-opacity-80`}
+					className={`${bottomIsDisplayed ? 'absolute -left-10 top-12 rounded-md bg-white p-4 shadow-back dark:bg-navbar-icons-background-d' : 'hidden bg-transparent px-2 dark:opacity-80 md:block'} text-sm focus:outline-none dark:text-white dark:placeholder-white dark:placeholder-opacity-80`}
 				/>
 			</Form>
 		</>
