d12ef0b64e5e79271f63b9c5e6a54c8913ae3a4a
onnymm <phr2807@gmail.com>
2024-08-16 12:55:25 -0700

    Mejorado el componente de prueba para tabla

--- src/test/ActionButtons.jsx
+++ src/test/ActionButtons.jsx
@@ -1,10 +1,11 @@
-import { EllipsisVerticalIcon, PencilIcon, TrashIcon } from "@heroicons/react/16/solid";
+import { EllipsisVerticalIcon, PencilIcon, ShareIcon, TrashIcon } from "@heroicons/react/16/solid";
 import IconButton from "../components/ui kit/buttons/IconButton";
 
 const ActionButtons = () => {
 
     return (
         <div className="flex flex-row gap-2">
+            <IconButton icon={ShareIcon} type={'primary'} size={'sm'}/>
             <IconButton icon={PencilIcon} type={'successSecondary'} size={'sm'}/>
             <IconButton icon={TrashIcon} type={'dangerSecondary'} size={'sm'}/>
             <IconButton icon={EllipsisVerticalIcon} type={'secondary'} size={'sm'}/>
