f479b569b8191441b9e7358efd4b6edbfb4d899e
onnymm <phr2807@gmail.com>
2024-08-16 09:58:53 -0700

    Elaborado componente de prueba

---
+++ src/test/ActionButtons.jsx
@@ -0,0 +1,15 @@
+import { EllipsisVerticalIcon, PencilIcon, TrashIcon } from "@heroicons/react/16/solid";
+import IconButton from "../components/ui kit/buttons/IconButton";
+
+const ActionButtons = () => {
+
+    return (
+        <div className="flex flex-row gap-2">
+            <IconButton icon={PencilIcon} type={'successSecondary'} size={'sm'}/>
+            <IconButton icon={TrashIcon} type={'dangerSecondary'} size={'sm'}/>
+            <IconButton icon={EllipsisVerticalIcon} type={'secondary'} size={'sm'}/>
+        </div>
+    )
+}
+
+export default ActionButtons;
