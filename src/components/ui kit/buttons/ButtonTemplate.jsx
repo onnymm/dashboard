3ed721266b962d2ce97f33f4d82c01c677b0d9c3
onnymm <phr2807@gmail.com>
2024-08-15 18:00:07 -0700

    Completada la base del botón para ui

--- src/components/ui kit/buttons/ButtonTemplate.jsx
+++ src/components/ui kit/buttons/ButtonTemplate.jsx
@@ -1,12 +1,45 @@
-const ButtonTemplate = ({ children, onClick, disabled }) => {
+const ButtonTemplate = ({
+    children, // Contenido del botón
+    onClick, // Función del botón
+    disabled, // Deshabilitado
+    type='secondary', // Tipo de botón
+    rounded, // Redondeo de los bordes del botón
+}) => {
+
+    const typeOptions = {
+        primary: 'bg-blue-500 text-white',
+        secondary: 'text-gray-500 hover:bg-white hover:text-gray-500 border border-gray-300 dark:text-white dark:hover:text-gray-500',
+        danger: 'bg-red-500 text-white',
+        success: 'bg-green-500 text-white',
+    }
+
+    const circleEffect = {
+        primary: 'bg-white/25',
+        secondary: 'bg-gray-600/25',
+        danger: 'bg-white/25',
+        success: 'bg-white/25',
+    }
+
+    const roundedOptions = {
+        min: '',
+        none: '-none',
+        sm: '-sm',
+        md: '-md',
+        xl: '-xl',
+        full: '-full',
+    }
+
+    const roundedClassName = `rounded${roundedOptions[rounded] || roundedOptions.xl}`
+    const typeClassName = typeOptions[type]
+
     const callback = (event) => {
-        createCircle(event);
+        createCircle(event, circleEffect[type]);
         onClick();
     }
 
     return (
         <button
-            className="p-2 rounded-xl bg-blue-400 overflow-hidden relative active:scale-95 transition-transform duration-150 disabled:grayscale disabled:transform-none"
+            className={`${roundedClassName} ${typeClassName} shadow-lg p-2 overflow-hidden relative active:scale-95 transition duration-150 disabled:bg-gray-400 disabled:text-white dark:disabled:bg-gray-400 dark:disabled:border-gray-400 dark:disabled:text-white disabled:transform-none`}
             onClick={(event) => callback(event)}
             disabled={disabled}
         >
@@ -17,7 +50,7 @@ const ButtonTemplate = ({ children, onClick, disabled }) => {
 
 export default ButtonTemplate;
 
-const createCircle = (event) => {
+const createCircle = (event, backgroundColor) => {
 
     // Obtención del elemento HTML
     const buttonElement = event.currentTarget;
@@ -32,8 +65,9 @@ const createCircle = (event) => {
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
 
-    // Adición de clase para estilización
+    // Adición de clases para estilización
     circle.classList.add("circle")
+    circle.classList.add(backgroundColor)
 
     // Definición de altura y ancho del círculo
     circle.style.width = `${buttonElement.offsetWidth}px`;
