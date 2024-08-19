e13527002d1ef2b64745c27bf3c6bc2064e4080f
onnymm <phr2807@gmail.com>
2024-08-19 09:30:45 -0700

    Merge con tables

---
+++ src/test/ActionButtons.jsx
@@ -0,0 +1,16 @@
+import { EllipsisVerticalIcon, PencilIcon, ShareIcon, TrashIcon } from "@heroicons/react/16/solid";
+import IconButton from "../components/ui kit/buttons/IconButton";
+
+const ActionButtons = () => {
+
+    return (
+        <div className="flex flex-row gap-2">
+            <IconButton icon={ShareIcon} type={'primary'} size={'sm'} disabled />
+            <IconButton icon={PencilIcon} type={'successSecondary'} size={'sm'}/>
+            <IconButton icon={TrashIcon} type={'dangerSecondary'} size={'sm'}/>
+            <IconButton icon={EllipsisVerticalIcon} type={'secondary'} size={'sm'}/>
+        </div>
+    )
+}
+
+export default ActionButtons;
