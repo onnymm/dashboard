18d0dd58e31e02b11bc77c3d2694a56406028cb8
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-21 15:02:27 -0600

    Se añadió esqueleto para las charts

--- src/components/skeletons/FallbackChart.jsx
+++ src/components/skeletons/FallbackChart.jsx
@@ -2,11 +2,11 @@ import { ChartBarSquareIcon } from '@heroicons/react/24/outline'
 
 const FallbackChart = () => {
 	return (
-		<div className='relative flex flex-grow animate-pulse items-center justify-center dark:bg-slate-600'>
+		<div className='transition-dark relative flex flex-grow animate-pulse items-center justify-center bg-slate-200 dark:bg-slate-600'>
 			{/* Background Icon with Pulsing Effect */}
-			<ChartBarSquareIcon className='size-16 dark:text-white dark:opacity-60' />
+			<ChartBarSquareIcon className='transition-dark size-16 opacity-50 dark:text-white dark:opacity-60' />
 			{/* Foreground Icon */}
-			<ChartBarSquareIcon className='absolute size-12 animate-ping dark:text-white' />
+			<ChartBarSquareIcon className='transition-dark absolute size-12 animate-ping opacity-60 dark:text-white' />
 		</div>
 	)
 }
