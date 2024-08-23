32cf46fa97ec39c95819844b577a704268dbc3b2
onnymm <phr2807@gmail.com>
2024-08-23 16:44:39 -0700

    Añadida funcionalidad de actualización instantánea en campo numérico

--- src/components/ui kit/inputs/NumericInput.jsx
+++ src/components/ui kit/inputs/NumericInput.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import InputTemplate from "./InputTemplate";
 
 const NumericInput = ({ defaultValue, valueHandle, min, max, instantUpdate }) => {
+    const [render, setRender] = useState(false)
+
     const sendValue = (e) => {
         let value = Number(e.target.value)
         if ( value < min) {
             value = min
+            setRender( (prevState) => (!prevState) );
         } else if ( value > max ) {
             value = max
+            setRender( (prevState) => (!prevState) );
         }
 
         valueHandle(value)
@@ -23,7 +28,7 @@ const NumericInput = ({ defaultValue, valueHandle, min, max, instantUpdate }) =>
     }
 
     return (
-        <InputTemplate type="number" defaultValue={defaultValue} keyDownCallbacks={keyDownCallbacks} min={min} max={max} onBlur={updateCallback} centerText />
+        <InputTemplate key={render} type="number" defaultValue={defaultValue} keyDownCallbacks={keyDownCallbacks} min={min} max={max} onBlur={updateCallback} centerText />
     )
 }
 
