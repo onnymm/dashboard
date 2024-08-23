b0b08ccee4ecbbc458cf15af3c95700a1d93d542
onnymm <phr2807@gmail.com>
2024-08-23 16:13:19 -0700

    Agregada opción de botón deshabilitado en botón de texto

--- src/components/ui kit/buttons/TextButton.jsx
+++ src/components/ui kit/buttons/TextButton.jsx
@@ -3,11 +3,12 @@ import ButtonTemplate from "./ButtonTemplate";
 const TextButton = ({
     children, // Texto del botón
     onClick, // Función del botón
-    type // Apariencia del botón
+    type, // Apariencia del botón
+    disabled=false
 }) => {
 
     return (
-        <ButtonTemplate type={type} size="text" onClick={onClick} rounded={"lg"}>
+        <ButtonTemplate type={type} size="text" onClick={onClick} rounded={"lg"} disabled={disabled}>
             {children}
         </ButtonTemplate>
     )
