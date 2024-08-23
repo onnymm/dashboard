bab3f1924ed16a753bb2c0261ac3f75468ac9102
onnymm <phr2807@gmail.com>
2024-08-23 16:53:05 -0700

    Modificación en componente experimental

--- src/test/ActionButtons.jsx
+++ src/test/ActionButtons.jsx
@@ -1,11 +1,11 @@
-import { EllipsisVerticalIcon, PencilIcon, ShareIcon, TrashIcon } from "@heroicons/react/16/solid";
+import { EllipsisVerticalIcon, EyeIcon, PencilIcon, TrashIcon } from "@heroicons/react/16/solid";
 import IconButton from "../components/ui kit/buttons/IconButton";
 
 const ActionButtons = () => {
 
     return (
         <div className="flex flex-row gap-2">
-            <IconButton icon={ShareIcon} type={'primary'} size={'sm'} disabled />
+            <IconButton icon={EyeIcon} type={'primary'} size={'sm'} />
             <IconButton icon={PencilIcon} type={'success'} size={'sm'}/>
             <IconButton icon={TrashIcon} type={'danger'} size={'sm'}/>
             <IconButton icon={EllipsisVerticalIcon} type={'secondary'} size={'sm'}/>
