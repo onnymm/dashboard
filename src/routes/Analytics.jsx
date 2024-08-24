79f71dc795477d0d3537cd612e6105b425b90c79
onnymm <phr2807@gmail.com>
2024-08-23 17:51:19 -0700

    Corrección en importación de componente inexistente

--- src/routes/Analytics.jsx
+++ src/routes/Analytics.jsx
@@ -1,9 +1,7 @@
-import Table from "../../components/ui kit/tables/Table"
 
 const Analytics = () => {
 	return (
 		<div className='flex h-screen flex-grow items-center justify-center text-center'>
-			<Table />
 		</div>
 	)
 }
