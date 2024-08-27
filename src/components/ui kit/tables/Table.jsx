641c0737b6bfa3704b7df5f7b4558787de029a8c
onnymm <phr2807@gmail.com>
2024-08-26 17:50:53 -0700

    Actualizado el componente de tabla

--- src/components/ui kit/tables/Table.jsx
+++ src/components/ui kit/tables/Table.jsx
@@ -1,6 +1,7 @@
 import { EyeIcon, FunnelIcon, TableCellsIcon, XMarkIcon } from "@heroicons/react/16/solid";
 import { useEffect, useRef, useState } from "react";
 import { filterData } from "../../../core/tablesFunctionality";
+import { useColumns } from "../../../custom hooks/useColumns";
 import IconTextButton from "../buttons/IconTextButton";
 import NumericInput from "../inputs/NumericInput";
 import SearchInput from "../inputs/SearchInput";
@@ -9,21 +10,28 @@ import TableContent from "./elements/TableContent";
 import TablePagination from "./pagination/TablePagination";
 
 const Table = ({
-    columnsToRender,
-    columns,
     data,
+    columns,
+    columnsToRender,
+    filters,
+    searchScope,
     itemsPerPage = 50,
-    filters
 }) => {
 
     // Referencia de componente de tabla para manipulación directa
     const tableDataRef = useRef(null);
 
+    // Estado para búsqueda
+    const [search, setSearch] = useState({ criteria: () => true })
     // Estado para filtros
     const [activeFilter, setActiveFilter] = useState(undefined);
 
-    // Filtrado de datos de la tabla
-    data = filterData(data, filters[activeFilter]);
+    // Filtrado de datos de la tabla por búsqueda
+    data = filterData(data, search);
+    // Filtrado de datos de la tabla por filtros
+    if ( filters ) {
+        data = filterData(data, filters[activeFilter]);
+    }
 
     // Función para limpiar filtros
     const cleanFilters = () => {
@@ -43,31 +51,49 @@ const Table = ({
         }, [data, page]
     );
 
-    // Reseteo a la página 1 en cambios de filtro
+    // Reseteo a la página 1 en cambios de filtro o búsqueda
     useEffect(
         () => {
             setPage(0)
-        }, [activeFilter]
+        }, [activeFilter, search]
     );
 
     return (
         <div className="flex flex-col w-full p-4 gap-4">
             <div className="flex flex-row gap-4 justify-start">
-                <SearchInput />
-                <Select icon={FunnelIcon} options={filters} activeOptions={activeFilter} setActiveOptions={setActiveFilter} activeIcon={FunnelIcon}>
-                    Filtrar por
-                </Select>
-                <IconTextButton icon={XMarkIcon} onClick={cleanFilters} disabled={activeFilter === undefined}>
-                    Limpiar filtros
-                </IconTextButton>
-                <Select icon={TableCellsIcon} options={availableColumns} activeOptions={activeColumns} setActiveOptions={toggleColumn} activeIcon={EyeIcon} multiOptional>
-                    Ver columnas
-                </Select>
+
+                {/* Campo de búsqueda */}
+                {searchScope && <SearchInput setSearch={setSearch} searchScope={searchScope} />}
+
+                {/* Sección de filtros */}
+                {
+                    filters &&
+                    <Select icon={FunnelIcon} options={filters} activeOptions={activeFilter} setActiveOptions={setActiveFilter} activeIcon={FunnelIcon}>
+                        Filtrar por
+                    </Select>
+                }
+                {
+                    filters &&
+                    <IconTextButton icon={XMarkIcon} onClick={cleanFilters} disabled={activeFilter === undefined}>
+                        Limpiar filtros
+                    </IconTextButton>
+                }
+
+                {/* Filtro de columnas */}
+                {
+                    availableColumns &&
+                    <Select icon={TableCellsIcon} options={availableColumns} activeOptions={activeColumns} setActiveOptions={toggleColumn} activeIcon={EyeIcon} multiOptional>
+                        Ver columnas
+                    </Select>
+                }
             </div>
             <div className="flex flex-row gap-4">
+                {/* Paginación superior de página */}
                 <TablePagination data={data} itemsPerPage={itemsPerPage} page={page} setPage={setPage} />
+                {/* Selector de página */}
                 <NumericInput defaultValue={page + 1} valueHandle={(value) => setPage(value - 1)} min={1} max={Math.trunc(data.length / itemsPerPage) + (data.length % itemsPerPage === 0 ? 0 : 1)} instantUpdate />
             </div>
+            {/* Contenido y diseño de la tabla */}
             <div className="w-auto flex flex-col p-2 shadow-md border border-gray-500/30 transition duration-100 rounded-xl h-max bg-white dark:bg-gray-800">
                 <div className="rounded-lg h-0 z-10 pointer-events-none">
                     <div className="w-full h-10 bottom-10 rounded-lg shadow-md dark:border-none"></div>
@@ -77,71 +103,13 @@ const Table = ({
                 </div>
             </div>
             <div className="flex flex-row gap-4">
+                {/* Paginación superior de página */}
                 <TablePagination data={data} itemsPerPage={itemsPerPage} page={page} setPage={setPage} />
+                {/* Selector de página */}
                 <NumericInput defaultValue={page + 1} valueHandle={(value) => setPage(value - 1)} min={1} max={Math.trunc(data.length / itemsPerPage) + (data.length % itemsPerPage === 0 ? 0 : 1)} instantUpdate />
             </div>
-            
         </div>
     )
-}
-
-const useColumns = (columnsArray, columnsToRender) => {
-    let columns = []
-
-    columnsArray.forEach(
-        (item) => {
-            columns.push(
-                {
-                    name: item.name,
-                    displayName: item.field_description,
-                }
-            )
-        }
-    )
-
-    let index = 0
-
-    const newColumns = Object.keys(columnsToRender).map(
-        (key) => {
-            if ( columnsToRender[key] === 'data' ) {
-                const column = columns.find((column) => column.name === key)
-                return column
-            } else {
-                const column = columnsToRender[key]
-                if ( column.visible !== undefined ) {
-                    column.index = index
-                    index++
-                }
-                console.log(column)
-                return column
-            }
-        }
-    )
-
-    const [activeColumns, setActiveColumns] = useState(newColumns)
-
-    const toggleColumn = (toggleIndex) => {
-        let columnsMap = [...activeColumns]
-        columnsMap.forEach(
-            (column) => {
-                if ( column.index === toggleIndex ) {
-                    column.visible = !column.visible
-                }
-            }
-        )
-        setActiveColumns(columnsMap)
-    }
-
-    const availableColumns = []
-    activeColumns.forEach(
-        (option) => {
-            if ( option.visible !== undefined ) {
-                availableColumns.push(option)
-            }
-        }
-    )
-
-    return { availableColumns, activeColumns, toggleColumn }
 }
 
 export default Table;
