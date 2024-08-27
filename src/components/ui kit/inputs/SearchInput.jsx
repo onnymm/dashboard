dcfec7fd3446f3f228e892cf756981d39525ec85
onnymm <phr2807@gmail.com>
2024-08-26 17:33:23 -0700

    Habilitado el botón de búsqueda

--- src/components/ui kit/inputs/SearchInput.jsx
+++ src/components/ui kit/inputs/SearchInput.jsx
@@ -1,10 +1,25 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import { buildSearchCriteria } from "../../../core/buildSearchCriteria";
 import InputTemplate from "./InputTemplate";
 
-const SearchInput = () => {
+const SearchInput = ({ setSearch, searchScope }) => {
+    // Función para filtro de datos
+    const filterData = (e) => {
+
+        // Generación del criterio de búsqueda
+        const criteria = buildSearchCriteria(e, searchScope)
+
+        // Asignación del estado con la función
+        setSearch({ criteria })
+    }
+
+    // Teclas de ejecución de búsqueda
+    const keyDownCallbacks = {
+        "Enter": filterData,
+    }
 
     return (
-        <InputTemplate type="search" icon={MagnifyingGlassIcon} placeholder={"Buscar"} />
+        <InputTemplate type="search" icon={MagnifyingGlassIcon} placeholder={"Buscar"} keyDownCallbacks={keyDownCallbacks} />
     )
 }
 
