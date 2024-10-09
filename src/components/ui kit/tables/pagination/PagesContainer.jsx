a2ef8a61e438e5c523185d877221883c7ef3f239
onnymm <phr2807@gmail.com>
2024-10-09 13:47:41 -0700

    Mejorado el componente contenedor de páginas para paginaciónd de tabla

--- src/components/ui kit/tables/pagination/PagesContainer.jsx
+++ src/components/ui kit/tables/pagination/PagesContainer.jsx
@@ -4,37 +4,38 @@ import PageButton from "./PageButton";
 const PagesContainer = ({
     pagesToShow,
     page,
-    setSelectPositionIndex
+    setSelectPositionIndex,
+    disabled,
 }) => {
 
     return (
-        <div className="flex relative">
+        <div className="relative sm:flex hidden">
             {/* Botón de página */}
             <div className="flex">
                 {
                     pagesToShow.length !== 0
                     ? pagesToShow.map(
                         (pageNum) => (
-                            <PageButton key={pageNum} value={pageNum} callback={() => setSelectPositionIndex(pageNum)} />
+                            <PageButton key={pageNum} value={pageNum} callback={() => setSelectPositionIndex(pageNum)} disabled={disabled} />
                         )
                     )
-                    : <PageButton value={0} callback={() => null} />
+                    : <PageButton value={0} callback={() => null} disabled={disabled} />
                 }
             </div>
             {/* Indicador de página */}
-            <div className="absolute h-12 shadow-md rounded-xl w-12 bg-main-500 duration-300 transition-transform" style={{transform: `translateX( calc(${pagesToShow.indexOf(page) !== -1 ? pagesToShow.indexOf(page) : 0} * 3rem ))`}}></div>
+            <div className="absolute bg-main-500 shadow-md rounded-xl size-12 sm:size-10 transition-transform duration-300" style={{transform: `translateX( calc(${pagesToShow.indexOf(page) !== -1 ? pagesToShow.indexOf(page) : 0} * 2.5rem ))`}}></div>
             {/* Contenedores de números */}
-            <div className="flex absolute pointer-events-none">
+            <div className="absolute flex pointer-events-none">
                 {
                     pagesToShow.length !== 0
                     ? pagesToShow.map(
                         (pageToShow, index) => (
-                            <div key={index} className={`${pageToShow === page ? "text-white" : ""} transition duration-200 flex w-12 h-12 justify-center items-center dark:text-white`}>
+                            <div key={index} className={`${pageToShow === page ? "text-white" : ""} ${disabled ? "text-gray-500/50 dark:text-gray-300/50" : ""} transition duration-200 flex sm:text-sm size-12 sm:size-10 justify-center items-center dark:text-white`}>
                                 {typeof pageToShow === "number" ? pageToShow + 1 : <EllipsisHorizontalIcon className="size-4" />}
                             </div>
                         )
                     )
-                    : <div className={"text-white transition duration-200 flex w-12 h-12 justify-center items-center dark:text-white"}>
+                    : <div className={"text-white transition duration-200 flex size-12 sm:size-10 sm:text-sm justify-center items-center dark:text-white"}>
                     1
                     </div>
                 }
