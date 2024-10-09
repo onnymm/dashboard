6ecc544b66a30f0178fe38444516f1096a606f68
onnymm <phr2807@gmail.com>
2024-10-09 14:29:02 -0700

    Añadidas funciones para métodos GET

--- src/api/get.js
+++ src/api/get.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 import { defaultDomain } from "../settings/dashboardSettings"
+import { buildGet } from "./buildAPIrequests"
+import { domains } from "../settings/backendDomains"
 
 export const getChartData = async (
     stateSetter, // Función useState para cambio de estado
@@ -18,9 +20,59 @@ export const getChartData = async (
     stateSetter(response.data)
 }
 
+export const getTableData = buildGet(
+    {
+        domain: domains.localdomain,
+        name: "list_data",
+        params: {
+            "model": "string",
+            "search_criteria": "pyExp",
+            "fields": "array",
+            "page": "number",
+            "items_per_page": "number",
+            "sortby": "string",
+            "ascending": "boolean",
+            "search": "objectList",
+        }
+    }
+)
+
+// Solicitud GET de prueba
+export const comisiones = buildGet(
+    {
+        domain: domains.localdomain,
+        name: "comisiones",
+        params: {
+            "page": "number",
+            "items_per_page": "number",
+            "search_criteria": "pyExp",
+            "sortby": "string",
+            "ascending": "boolean",
+            "search": "objectList",
+        }
+    }
+)
+
+// Métodos GET para pruebas
 export const getUsers = async (stateSetter) => {
     // Se obtiene la información desde el API
     const response = await axios.get(`${defaultDomain}fields`)
     // Se retorna la información transformada
+    stateSetter(response.data)
+}
+export const getSaleOrders = async (stateSetter) => {
+    // Se obtiene la información desde el API
+    const response = await axios.get(`${defaultDomain}sale_orders`)
+    // Se retorna la información transformada
+    stateSetter(response.data)
+}
+export const getProductsStock = async (stateSetter) => {
+    // Se obtiene la información desde el API
+    const response = await axios.get(`${defaultDomain}products_stock`)
+    // Se retorna la información transformada
+    stateSetter(response.data)
+}
+export const getStats = async (stateSetter) => {
+    const response = await axios.get(`${defaultDomain}get_stats`)
     stateSetter(response.data)
 }
